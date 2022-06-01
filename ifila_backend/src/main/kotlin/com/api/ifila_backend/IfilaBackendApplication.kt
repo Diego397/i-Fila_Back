80ed399341b477e5bbec2b02dc9247dfec8a06fc
marcus2809 <marcusvinicius.paz2001@alu.ufc.br>
2022-05-31 21:19:15 -0300

    configuração inicial spring boot

---
+++ ifila_backend/src/main/kotlin/com/api/ifila_backend/IfilaBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.api.ifila_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IfilaBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<IfilaBackendApplication>(*args)
+}
---
+++ ifila_backend/src/test/kotlin/com/api/ifila_backend/IfilaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.ifila_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IfilaBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
