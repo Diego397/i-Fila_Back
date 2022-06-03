6f82341406e75aac1af4b0853816ec8dba243361
marcus2809 <marcusvinicius.paz2001@alu.ufc.br>
2022-06-02 23:46:18 -0300

    Configuração do banco de dados concluida

--- ifila_backend/src/main/kotlin/com/api/ifila_backend/IfilaBackendApplication.kt
+++ ifila_backend/src/main/kotlin/com/api/ifila_backend/IfilaBackendApplication.kt
@@ -2,9 +2,11 @@ package com.api.ifila_backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class IfilaBackendApplication
+class IfilaBackendApplication()
 
 fun main(args: Array<String>) {
 	runApplication<IfilaBackendApplication>(*args)
---
+++ ifila_backend/src/main/kotlin/com/api/ifila_backend/models/usuarioModel.kt
@@ -0,0 +1,31 @@
+package com.api.ifila_backend.models
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.UUID
+import javax.persistence.*
+
+@Entity
+@Table(name = "TB_USUARIO")
+class usuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id:UUID? = null
+
+    @Column(nullable = false, length = 70)
+    lateinit var nome:String
+
+    @Column(nullable = false)
+    lateinit var dataDeNascimento:LocalDate
+
+    @Column(nullable = true, length = 350)
+    lateinit var endereco:String
+
+    @Column(nullable = false, length = 100)
+    lateinit var email:String
+
+    @Column(nullable = false, length = 20)
+    lateinit var numeroCelular:String
+
+}
